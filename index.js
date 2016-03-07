ed0fbea1386ef4e5270ce802d39ffbe4247a51c5
Danny Bauman <dannybauman@gmail.com>
2016-03-07 15:09:44 -0800

    pipe the incoming audio data to ffmpeg's segmenter

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ module.exports = WebSocketMirror
 
 var ws = require('ws')
 var inherits = require('inherits')
+var cp = require('child_process')
+var wsstream = require('websocket-stream')
 
 inherits(WebSocketMirror, ws.Server)
 
@@ -21,12 +23,35 @@ WebSocketMirror.prototype._onconnection = function (socket) {
   var channel = path[0]
   var action = path[1]
   if (action === 'publish') {
+    this._startRecording(channel, socket)
     socket.on('message', this._onmessage.bind(this, channel))
   } else if (action === 'subscribe') {
     this.channels[channel] = this.channels[channel] || []
     this.channels[channel].push(socket)
     socket.on('close', this._onsubscriberclose.bind(this, channel, socket))
   }
+}
+
+WebSocketMirror.prototype._startRecording = function (channel, socket) {
+  var ffmpegProc = cp.spawn("ffmpeg",[
+      "-re",
+      "-i", "pipe:0", // input from STDIN
+      //"-i", "A.wav",
+      "-map", "0:0",
+      "-f", "segment",
+      "-segment_time", "10",
+      "-segment_list", './' + channel + ".m3u8",
+      "-segment_format", "mpgets", './' + channel + "-output%3d.wav"
+  ])
+
+  ffmpegProc.stderr.pipe(process.stderr)
+
+  ffmpegProc.on('error', function (err) {
+    console.error(err)
+  })
+
+  var stream = wsstream(socket)
+  stream.pipe(ffmpegProc.stdin)
 }
 
 WebSocketMirror.prototype._onmessage = function (channel, message) {
